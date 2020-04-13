1734be8128f07e4736f54e2fb69a4ddbb2b79be4
miaboloix <MbMv2017>
2020-04-13 15:40:34 -0400

    tests depend on 2017/2018/2019 -- will delete later

--- static/js/redux/ui/modals/user_settings_modal.jsx
+++ static/js/redux/ui/modals/user_settings_modal.jsx
@@ -303,6 +303,9 @@ class UserSettingsModal extends React.Component {
                 name="form-field-name"
                 value={this.props.userInfo.class_year}
                 options={[
+                                    { value: 2020, label: 2017 },
+                                    { value: 2021, label: 2018 },
+                                    { value: 2022, label: 2019 },
                                     { value: 2020, label: 2020 },
                                     { value: 2021, label: 2021 },
                                     { value: 2022, label: 2022 },
