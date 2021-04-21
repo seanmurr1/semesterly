1c59e028f7289b96e59bed7274e1a5096493052a
Rishibrata Biswas <rbis1001@gmail.com>
2021-04-21 17:53:44 -0400

    Made Frontend components of modal, now working on connecting with backend.

--- static/js/redux/ui/modals/add_advisor_modal.jsx
+++ static/js/redux/ui/modals/add_advisor_modal.jsx
@@ -15,8 +15,20 @@ GNU General Public License for more details.
 import PropTypes from 'prop-types';
 import React from 'react';
 import Modal from 'boron/WaveModal';
+import {getTranscriptCommentsBySemester} from "../../constants/endpoints";
+import Cookie from "js-cookie";
 
 class AddAdvisorModal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      jhed: '',
+      first_name: '',
+      last_name: '',
+    };
+  }
+
   componentDidMount() {
     if (this.props.isVisible) {
       this.modal.show();
@@ -29,20 +41,48 @@ class AddAdvisorModal extends React.Component {
     }
   }
 
+  sendJHED(event) {
+    this.setState({jhed: event.target.value});
+  }
+
+  sendFirstName(event) {
+    this.setState({first_name: event.target.value});
+  }
+
+  sendLastName(event) {
+    this.setState({last_name: event.target.value});
+  }
+
+  submitContent(semesterName, semesterYear) {
+    if (this.state.comment !== '') {
+      fetch(getTranscriptCommentsBySemester(semesterName, semesterYear), {
+        method: 'POST',
+        headers: {
+          'X-CSRFToken': Cookie.get('csrftoken'),
+          accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          jhed: this.props.userInfo.jhed,
+          timestamp: new Date(Date.now()),
+          content: this.state.comment,
+        })
+      })
+        .then(() => this.setState({comment: this.state.comment = '', submitted: !this.state.submitted}));
+    }
+  }
+
   render() {
+    const { jhed, first_name, last_name } = this.state;
+
     const modalHeader =
       (<div className="modal-content">
         <div className="modal-header">
-          <h1>Import Data from SIS</h1>
+          <h1>Add New Advisors</h1>
           <div className="modal-close" onClick={() => this.modal.hide()}>
             <i className="fa fa-times" />
           </div>
         </div>
-      </div>);
-    const modalBody =
-      (<div className="modal-body">
-        <h3>In order to connect with your advisors through Semester.ly,
-          you must agree to Semester.ly importing your data from SIS.</h3>
       </div>);
     const modalStyle = {
       width: '100%',
@@ -59,15 +99,33 @@ class AddAdvisorModal extends React.Component {
         }}
       >
         {modalHeader}
-        {modalBody}
-        <div className="go-to-sis__container">
-          <a
-            className="btn abnb-btn secondary"
-            href="https://self-dev.sis.jhu.edu/sandbox/sswf/go/?semesterlyexport=1"
-          >
-            <span>Import from SIS</span>
-          </a>
-        </div>
+        <p><br/></p>
+        <form action="#1">
+          <textarea
+            className="cf-input"
+            rows="1" placeholder="Enter Advisor JHED"
+            value={jhed}
+            onChange={event => this.sendJHED(event)}
+          />
+          <textarea
+            className="cf-input"
+            rows="1" placeholder="Enter Advisor First Name"
+            value={first_name}
+            onChange={event => this.sendFirstName(event)}
+          />
+          <textarea
+            className="cf-input"
+            rows="1" placeholder="Enter Advisor Last Name"
+            value={last_name}
+            onChange={event => this.sendLastName(event)}
+          />
+          <p><br/></p>
+          <input
+            className="send-btn"
+            type="submit"
+            value="Send Info"
+          />
+        </form>
       </Modal>
     );
   }
