ae573c1002639666751ba3b9e2458506b3df27a9
Mike Sellers <mike.sellers@jhu.edu>
2021-07-28 18:46:54 -0400

    SEM-157 - Implementing updated subdomain_middleware.py and ingress-config-map.yml to handle Azure AGW and Kube forwarding for HTTP headers

--- semesterly/middleware/subdomain_middleware.py
+++ semesterly/middleware/subdomain_middleware.py
@@ -17,37 +17,29 @@ from parsing.schools.active import ACTIVE_SCHOOLS
 class SubdomainMiddleware(object):
     def process_request(self, request):
 
-        # Dump all META
-        for item in request.META.items():
-            logging.error(item)
-
         # Define domain suffixes for non-prod environments
-        
-        nonprod_suffixes = ("-dev", "-test", "-stage", "-prod")
-        prod_suffixes = ("sem", "semester")
+        nonprod_suffixes = ("-dev", "-test", "-stage", "-prod", "sem")
+
+        # Define domain suffixes for prod environments
+        prod_suffixes = ("semester")
 
         if 'HTTP_X_ORIGINAL_HOST' in request.META:
             subdomain = request.META.get('HTTP_X_ORIGINAL_HOST', '')
         else:
             subdomain = request.META.get('HTTP_HOST', '')
 
-        logging.error('SUBDOMAIN')
-        logging.error(subdomain)
-
         subdomain = subdomain.split('.')[0].strip().lower()
-
-        logging.error("FOLLOWING SPLIT")
-        logging.error(subdomain)
 
         if subdomain in ACTIVE_SCHOOLS:
             request.subdomain = subdomain
             logging.error("ACTIVE SCHOOLS")
+
         elif subdomain.endswith(nonprod_suffixes):
             # Default to JHU for non-prod URLs for ease of setup/testing
             request.subdomain = "jhu"
-            logging.error("NONPROD_SUFFIXES")
+
         elif subdomain in (prod_suffixes):
             request.subdomain = None
-            logging.error("PROD_SUFFIXES")
+
         else:
             request.subdomain = None
