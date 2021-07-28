ec0624ca3423814b7a45a55dfd64a1801614b8d8
Mike Sellers <mike.sellers@jhu.edu>
2021-07-28 13:37:37 -0400

    SEM-157 - Investigating headers

--- semesterly/middleware/subdomain_middleware.py
+++ semesterly/middleware/subdomain_middleware.py
@@ -17,18 +17,27 @@ class SubdomainMiddleware(object):
     def process_request(self, request):
         # Define domain suffixes for non-prod environments
         nonprod_suffixes = ("-dev", "-test", "-stage", "-prod")
+        prod_suffixes = ("sem", "semester")
 
         if 'X-Original-Host' in request.META:
             subdomain = request.META.get('X-Original-Host', '')
         else:
             subdomain = request.META.get('HTTP_HOST', '')
-        
+
+        print ("XOH compared to HTTP_HOST:")
+        print (subdomain)
+
         subdomain = subdomain.split('.')[0].strip().lower()
+
+        print ("Following split")
+        print (subdomain)
 
         if subdomain in ACTIVE_SCHOOLS:
             request.subdomain = subdomain
         elif subdomain.endswith(nonprod_suffixes):
             # Default to JHU for non-prod URLs for ease of setup/testing
             request.subdomain = "jhu"
+        elif subdomain in (prod_suffixes):
+            request.subdomain = None
         else:
             request.subdomain = None
