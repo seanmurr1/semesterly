26243a9783a999aa1560ca22fc381515b94f0439
Mike Sellers <mike.sellers@jhu.edu>
2021-07-28 17:27:54 -0400

    SEM-157 - Better view of META

--- semesterly/middleware/subdomain_middleware.py
+++ semesterly/middleware/subdomain_middleware.py
@@ -17,32 +17,37 @@ from parsing.schools.active import ACTIVE_SCHOOLS
 class SubdomainMiddleware(object):
     def process_request(self, request):
 
-
+        # Dump all META
+        for item in request.META.items():
+            logging.error(item)
 
         # Define domain suffixes for non-prod environments
         
         nonprod_suffixes = ("-dev", "-test", "-stage", "-prod")
         prod_suffixes = ("sem", "semester")
 
-        if 'HTTP_X_FORWARDED_HOST' in request.META:
-            subdomain = request.META.get('HTTP_X_FORWARDED_HOST', '')
+        if 'HTTP_X_Original_Host' in request.META:
+            subdomain = request.META.get('HTTP_X_Original_Host', '')
         else:
             subdomain = request.META.get('HTTP_HOST', '')
 
-        logging.error('XOH compared to HTTP_X_FORWARDED_HOST: ')
+        logging.error('SUBDOMAIN')
         logging.error(subdomain)
 
         subdomain = subdomain.split('.')[0].strip().lower()
 
-        logging.error("Following split")
+        logging.error("FOLLOWING SPLIT")
         logging.error(subdomain)
 
         if subdomain in ACTIVE_SCHOOLS:
             request.subdomain = subdomain
+            logging.error("ACTIVE SCHOOLS")
         elif subdomain.endswith(nonprod_suffixes):
             # Default to JHU for non-prod URLs for ease of setup/testing
             request.subdomain = "jhu"
+            logging.error("NONPROD_SUFFIXES")
         elif subdomain in (prod_suffixes):
             request.subdomain = None
+            logging.error("PROD_SUFFIXES")
         else:
             request.subdomain = None
